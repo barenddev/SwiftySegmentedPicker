2ae13bb3f86e30875f1065803641c9a6f59e838f
Brian Arend <brian.arend@kubra.com>
2024-06-15 08:42:45 -0700

    segment sizes update flow improvement

--- Sources/SegmentedPicker/SegmentedPicker.swift
+++ Sources/SegmentedPicker/SegmentedPicker.swift
@@ -7,14 +7,15 @@
 
 import SwiftUI
 
+
 public struct SegmentedPicker<Element, Content, Selection>: View
     where
     Content: View,
     Selection: View {
-
+    
     public typealias Data = [Element]
-
-    @State private var frames: [CGRect]
+    
+    @State private var segmentSizes: [Data.Index: CGSize] = [:]
     @Binding private var selectedIndex: Data.Index?
 
     private let data: Data
@@ -32,19 +33,18 @@ public struct SegmentedPicker<Element, Content, Selection>: View
         self.content = content
         self.selection = selection
         self._selectedIndex = selectedIndex
-        self._frames = State(wrappedValue: Array(repeating: .zero,
-                                                 count: data.count))
         self.selectionAlignment = selectionAlignment
     }
+    
 
     public var body: some View {
         ZStack(alignment: Alignment(horizontal: .horizontalCenterAlignment,
                                     vertical: selectionAlignment)) {
 
-            if let selectedIndex = selectedIndex {
+            if let index = selectedIndex {
                 selection()
-                    .frame(width: frames[selectedIndex].width,
-                           height: frames[selectedIndex].height)
+                    .frame(width: selectionSize(at: index).width,
+                           height: selectionSize(at: index).height)
                     .alignmentGuide(.horizontalCenterAlignment) { dimensions in
                         dimensions[HorizontalAlignment.center]
                     }
@@ -57,14 +57,41 @@ public struct SegmentedPicker<Element, Content, Selection>: View
                     )
                     .buttonStyle(PlainButtonStyle())
                     .background(GeometryReader { proxy in
-                        Color.clear.onAppear { frames[index] = proxy.frame(in: .global) }
+                        Color.clear.preference(
+                            key: SegmentSizePreferenceKey.self,
+                            value: SegmentSize(index: index, size: proxy.size)
+                        )
                     })
+                    .onPreferenceChange(SegmentSizePreferenceKey.self) { segment in
+                        segmentSizes[segment.index] = segment.size
+                    }
                     .alignmentGuide(.horizontalCenterAlignment,
                                     isActive: selectedIndex == index) { dimensions in
                         dimensions[HorizontalAlignment.center]
                     }
                 }
             }
+        }
+    }
+    
+    private func selectionSize(at index: Data.Index) -> CGSize {
+        segmentSizes[index] ?? .zero
+    }
+}
+
+private extension SegmentedPicker {
+    struct SegmentSize: Equatable {
+        let index: Int
+        let size: CGSize
+    }
+    
+    struct SegmentSizePreferenceKey: PreferenceKey {
+        static var defaultValue: SegmentSize { SegmentSize(index: .zero, size: .zero) }
+        
+        static func reduce(value: inout SegmentSize,
+                           nextValue: () -> SegmentSize) {
+            
+            value = nextValue()
         }
     }
 }
